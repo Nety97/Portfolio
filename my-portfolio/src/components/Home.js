3bdceaa71d84be3ace6b6b6f2ce14f41337ed5bc
Nety <netanielw@gmail.com>
2021-10-15 17:00:13 +0300

    portfolio

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -10,7 +10,7 @@ function Home() {
        
         <div className='home'>
           <Fade left>
-            <h1>Hi Netaniel Wahinch here <br/> and I'm a Full Stack Web Developer</h1>
+            <h1>Hi Netaniel Wahnich here <br/> and I'm a Full Stack Web Developer</h1>
           </Fade>
         </div>
         <div className='curve2'></div>
